f81f586ac91c1b2f63318ea2471d01623548f353
vladimir <joinmeone@gmail.com>
2026-04-27 21:14:18 +0300

    Обновил комментарии

--- src/app/main.py
+++ src/app/main.py
@@ -20,7 +20,7 @@ if __name__ == "__main__": #Для докер хост используем ну
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=8001,
         reload=True,
         log_level="info"
 
